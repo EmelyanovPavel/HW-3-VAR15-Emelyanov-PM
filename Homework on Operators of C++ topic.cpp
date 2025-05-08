f87442b3154eb3ab218701339d4a0cfd35e6a3cb
EmelyanovPavel <gdetoty@list.ru>
2025-05-08 16:32:42 +0300

    Update Homework on Operators of C++ topic.cpp
    Написаны коды более понятно.

--- Homework on Operators of C++ topic.cpp
+++ Homework on Operators of C++ topic.cpp
@@ -1,134 +1,144 @@
+// Topic 3. Operators of C++.
 #include <iostream>
 #include <cmath>
 
 // Exercise 1. 
 // Given a point on the plane with coordinates (x, y). Output one of the messages (Yes, No, At the border) depending on whether the point lies inside the shaded area, outside the shaded area, or on its border. 
-// The area is represented as follows:
-// Option 15. y = -x
-// link to the picture: 
+// Areas are defined graphically as follows:
+// 
+// 15) y = -x
+// A link to the picture: 
 // https://private-user-images.githubusercontent.com/35574513/439328361-ddeec275-8696-4afb-8360-38e48b4d8ca1.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDYwOTE5ODcsIm5iZiI6MTc0NjA5MTY4NywicGF0aCI6Ii8zNTU3NDUxMy80MzkzMjgzNjEtZGRlZWMyNzUtODY5Ni00YWZiLTgzNjAtMzhlNDhiNGQ4Y2ExLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA1MDElMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwNTAxVDA5MjgwN1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWFjNGZkMGQyMDc0OWZiMmVmNmY4NDRjYzBhN2M3OGQ4ZGY4NzEwYWQyNWQ2N2QzNDg3NTYwZjg0NTRjYTgyYjUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.FCx82NZtkscX8eKoeXuR393VXbSYVHeJY48ikl87ah8
 
 void task1()
 {
-    constexpr auto rr = 100;
-    constexpr auto eps = 1e-12; //the eps constant equal to 1e-12 is used to check for zero so as not to divide by 0
-    
+    double r = 100.0;
+    const auto eps = 1e-12;
+
     double x, y;
+
+    std::cout << "Enter the point coordinates (x, y): ";
     std::cin >> x >> y;
-    
-    const auto a = x * x + y * y;
-    const auto b = y + x;
-    
-    if (fabs(b) < eps && a <= rr || fabs(a - rr) < eps && 0 <= b) {
+
+    int a = x * x + y * y;
+    int b = y + x;
+
+    std::cout << "Answer: ";
+
+    if (fabs(b) < eps && a <= r || fabs(a - r) < eps && 0 <= b) {
+
         std::cout << "At the border";
-    } else if (rr < a || b < 0) {
+
+    } else if (r < a || b < 0) {
+
         std::cout << "No";
+
     } else {
+
         std::cout << "Yes";
     }
-    
-    std::cout << std::endl; 
-    //all code works correctly
+
+    std::cout << std::endl;
 }
 
-//Exercise 2 
-//15) A feature of a geometric shape is given: c is a circle, r is a rectangle, t is a triangle. 
-//Display the perimeter and area of a given shape on the screen (request the data required for 
-//calculations from the user)
+//Exercise 2.
+// Create a program.
+// 
+//15) A feature of a geometric shape is given: 
+// c - circle; 
+// r - rectangle; 
+// t - triangle. 
+// 
+//Display the perimeter and area of a given shape on the screen 
+// (request the data required for calculations from the user)
 
 void task2()
 {
     char figure;
-    double perimeter = 0.0;
+
+    double perimeter = 0.0; 
     double area = 0.0;
     const double PI = 3.14;
 
     std::cout << "Symbol: ";
     std::cin >> figure;
 
-    switch (figure) {
+    switch (figure) 
+    {
+
     case 'c':
-        //declaring the data for entering
+
         double radius;
-        std::cout << "Radius: ";
+
+        std::cout << "Enter a radius: ";
         std::cin >> radius;
 
-        //calculating a perimeter and area
         perimeter = 2 * PI * radius;
         area = PI * radius * radius;
+
         break;
 
     case 'r':
-        //declaring the data for entering
+
         double a, b;
-        std::cout << "Rectangle sides: ";
+
+        std::cout << "Enter the rectangle sides: ";
         std::cin >> a >> b;
 
-        //calculating a perimeter and area
         perimeter = 2 * (a + b);
         area = a * b;
+
         break;
 
     case 't':
-        //declaring the data for calculations and entering
+
         double p, x, y, z;
-        std::cout << "Triangle sides: ";
+
+        std::cout << "Enter the triangle sides: ";
         std::cin >> x >> y >> z;
 
-        //calculating a perimeter and area
         perimeter = x + y + z;
-        p = perimeter / 2; //calculating a semiperimeter
+
+        p = perimeter / 2; 
         area = sqrt(p * (p - x) * (p - y) * (p - z));
+
         break;
-        
 
     default:
+
         std::cout << "Parameters are unknown!" << std::endl;
 
     }
-    std::cout << "Perimeter: " << perimeter << std::endl;
-    std::cout << "Area: " << area << std::endl;
+    std::cout << "Perimeter = " << perimeter << ", area = " << area << std::endl;
 }
 
-// Exercise 3 - Display on the screen...
+// Exercise 3.
+// Display on the screen...
+// 
 // 15) all even numbers in the range from A to B, multiples of three (A <= B)
 
 void task3()
 {
     int a, b;
+
+    std::cout << "Enter A and B: \n";
     std::cin >> a >> b;
-    
-    std::cout << "FOR...\n";
-    for (int i = a; i <= b; i++)
+
+    std::cout << "Numbers: ";
+
+    for (int i = a; i < b; i++)
     {
         if (i % 3 == 0 && i % 2 == 0)
         {
             std::cout << i << " ";
         }
     }
-    
-    int i2 = a;
-    std::cout << "\nWHILE...\n";
-    while(i2 <= b) {
-        if(i2 % 3 == 0 && i2 % 2 == 0) {
-            std::cout << i2 << " ";
-        }
-        i2++;
-    }
-    
-    int i3 = a;
-    std::cout << "\nDO... WHILE...\n";
-    do {
-        if(i3 % 3 == 0 && i3 % 2 == 0) {
-            std::cout << i3 << " ";
-        }
-        i3++;
-        
-    } while(i3 <= b);
     std::cout << std::endl;
 }
 
-//Exercise 4. Display the numbers in the form of the following tables
+//Exercise 4. 
+//Display the numbers in the form of the next tables:
+// 
 //15)
 //1
 //0
@@ -145,32 +155,37 @@ void newPrint(int k, int n)
 {
     for (int i = 1; i <= n; ++i)
     {
-        std::cout << k << " "; 
+        std::cout << k << " ";
     }
-        
+
 }
 
 void task4()
 {
-    int numbers_number = 5;
-
-    for (int i = 1; i <= numbers_number; ++i)
+    for (int i = 1; i <= 5; ++i)
     {
         newPrint(i, i);
         std::cout << "\n";
+
         newPrint(0, i);
         std::cout << "\n";
     }
 }
 
-//Exercise 5
+//Exercise 5.
+// Construct a table of values of the function y=f(x) for x ∈ [a, b] in increments of h. 
+// If the function is not defined at some point x, display a message about it.
+// 
 //15) y = ln|3x|√(2x^5 - 1)
 
-double calculateFunction(double x) {
-    if (x == 0 || (2 * pow(x, 5) - 1) < 0)
-        return NAN; 
+double calculateFunction(double x) 
+{
+    if (x == 0 || (2 * pow(x, 5) - 1) < 0) 
+    {
+        return NAN;
+    }
 
-    return log(fabs(3 * x)) * sqrt(2 * pow(x, 5) - 1);
+    return log(abs(3 * x)) * sqrt(2 * pow(x, 5) - 1);
 }
 
 void task5()
@@ -179,8 +194,10 @@ void task5()
 
     std::cout << "Begin of interval (a): ";
     std::cin >> a;
+
     std::cout << "End of interval (b): ";
     std::cin >> b;
+
     std::cout << "Step (h): ";
     std::cin >> h;
 
@@ -201,18 +218,21 @@ void task5()
         if (std::isnan(y))
         {
             std::cout << "x = " << x << ": function isn't defined!" << std::endl;
-        }
-        else {
+
+        } else {
+
             std::cout << std::fixed << "x = " << x << "\ty = " << y << std::endl;
         }
     }
 
 }
 
-//Exercise 6 
+//Exercise 6. 
+//Construct a table of values of the function y=f(x) for x ∈ [a, b] in increments of h. 
+// 
 //15)y = {1, if(x - 1) < 1;
 //        0, if(x - 1) = 1;
-//        -1, if(x - 1) > 1}
+//       -1, if(x - 1) > 1}
 
 void task6()
 {
@@ -224,16 +244,20 @@ void task6()
     std::cout << "x\ty\n";
     std::cout << "---\t---\n";
 
-    for (int x = -3; x <= 5; x++) {
+    for (int x = -3; x <= 5; x++) 
+    {
         int y;
 
-        if ((x - 1) < 1) {
+        if ((x - 1) < 1) 
+        {
             y = 1;
-        }
-        else if ((x - 1) == 1) {
+
+        } else if ((x - 1) == 1) {
+
             y = 0;
-        }
-        else {
+
+        } else {
+
             y = -1;
         }
 
